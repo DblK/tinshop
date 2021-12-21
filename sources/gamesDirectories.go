e452e4960e842e7722184c7961c029daeb6b4138
DblK <admin@dblk.org>
2021-12-21 17:39:07 +0100

    Beginning to implement watch directory properly

--- sources/gamesDirectories.go
+++ sources/gamesDirectories.go
@@ -1,15 +1,18 @@
 package sources
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	collection "github.com/dblk/tinshop/gamescollection"
 	"github.com/dblk/tinshop/repository"
 	"github.com/dblk/tinshop/utils"
+	"gopkg.in/fsnotify.v1"
 )
 
 func loadGamesDirectories(directories []string, singleSource bool) {
@@ -28,6 +31,10 @@ func loadGamesDirectories(directories []string, singleSource bool) {
 
 func loadGamesDirectory(directory string) error {
 	log.Printf("Loading games from directory '%s'...\n", directory)
+
+	// Add watcher for directories
+	watchDirectory(directory)
+
 	var newGameFiles []repository.FileDesc
 	// Walk through games directory
 	err := filepath.Walk(directory,
@@ -50,6 +57,9 @@ func loadGamesDirectory(directory string) error {
 						log.Println("Ignoring file because parsing failed", path)
 					}
 				}
+			} else if info.IsDir() && path != directory {
+				// TODO: Add watcher for the sub-directory
+				fmt.Println("Need to watch sub directory", path)
 			}
 			return nil
 		})
@@ -81,4 +91,55 @@ func downloadLocalFile(w http.ResponseWriter, r *http.Request, game, path string
 	} else {
 		http.ServeContent(w, r, game, time.Time{}, f)
 	}
+}
+
+func watchDirectory(directory string) {
+	fmt.Println("Watching directory", directory)
+
+	initWG := sync.WaitGroup{}
+	initWG.Add(1)
+	go func() {
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer watcher.Close()
+
+		eventsWG := sync.WaitGroup{}
+		eventsWG.Add(1)
+		go func() {
+			for {
+				select {
+				case event, ok := <-watcher.Events:
+					if !ok { // 'Events' channel is closed
+						eventsWG.Done()
+						return
+					}
+					const writeOrCreateMask = fsnotify.Write | fsnotify.Create
+					if event.Op&writeOrCreateMask != 0 {
+						fmt.Println("Changes", event)
+					} else if event.Op&fsnotify.Remove != 0 {
+						fmt.Println("Remove file", event)
+					} else if event.Op&fsnotify.Rename == fsnotify.Rename {
+						log.Printf("Rename: %s: %s", event.Op, event.Name)
+					}
+
+				case err, ok := <-watcher.Errors:
+					if ok { // 'Errors' channel is not closed
+						log.Printf("watcher error: %v\n", err)
+					}
+					eventsWG.Done()
+					return
+				}
+			}
+		}()
+		errWatcher := watcher.Add(directory)
+		initWG.Done()   // done initializing the watch in this go routine, so the parent routine can move on...
+		eventsWG.Wait() // now, wait for event loop to end in this go-routine...
+		if errWatcher != nil {
+			eventsWG.Done()
+		}
+	}()
+	initWG.Wait() // make sure that the go routine above fully ended before returning
+	fmt.Println("end of watch function")
 }
--- sources/sources.go
+++ sources/sources.go
@@ -1,14 +1,17 @@
+// @title tinshop Sources
+
+// @BasePath /sources/
+
+// Package sources provides management of various sources
 package sources
 
 import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 
 	"github.com/dblk/tinshop/repository"
 	"github.com/dblk/tinshop/utils"
-	"gopkg.in/fsnotify.v1"
 )
 
 var gameFiles []repository.FileDesc
@@ -19,12 +22,6 @@ func OnConfigUpdate(cfg repository.Config) {
 	gameFiles = make([]repository.FileDesc, 0)
 	loadGamesDirectories(cfg.Directories(), len(cfg.NfsShares()) == 0)
 	loadGamesNfsShares(cfg.NfsShares())
-
-	// Add watcher for directories
-	if len(cfg.Directories()) != 0 {
-		// TODO: Make loop
-		watchDirectory(cfg.Directories()[0])
-	}
 }
 
 // BeforeConfigUpdate from all sources
@@ -65,54 +62,4 @@ func DownloadGame(gameID string, w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotImplemented)
 		log.Printf("The type '%s' is not implemented to download game", GetFiles()[idx].HostType)
 	}
-}
-
-func watchDirectory(directory string) {
-	fmt.Println("Watching directory", directory)
-
-	initWG := sync.WaitGroup{}
-	initWG.Add(1)
-	go func() {
-		watcher, err := fsnotify.NewWatcher()
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer watcher.Close()
-
-		eventsWG := sync.WaitGroup{}
-		eventsWG.Add(1)
-		go func() {
-			for {
-				select {
-				case event, ok := <-watcher.Events:
-					if !ok { // 'Events' channel is closed
-						eventsWG.Done()
-						return
-					}
-					const writeOrCreateMask = fsnotify.Write | fsnotify.Create
-					if event.Op&writeOrCreateMask != 0 {
-						fmt.Println("Changes", event)
-					} else if event.Op&fsnotify.Remove != 0 {
-						fmt.Println("Remove file")
-						eventsWG.Done()
-						return
-					} else if event.Op&fsnotify.Rename == fsnotify.Rename {
-						log.Printf("Rename: %s: %s", event.Op, event.Name)
-					}
-
-				case err, ok := <-watcher.Errors:
-					if ok { // 'Errors' channel is not closed
-						log.Printf("watcher error: %v\n", err)
-					}
-					eventsWG.Done()
-					return
-				}
-			}
-		}()
-		watcher.Add(directory)
-		initWG.Done()   // done initializing the watch in this go routine, so the parent routine can move on...
-		eventsWG.Wait() // now, wait for event loop to end in this go-routine...
-	}()
-	initWG.Wait() // make sure that the go routine above fully ended before returning
-	fmt.Println("end of watch function")
 }
