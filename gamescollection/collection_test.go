d8c0e90253f90d2c5779e11a374f43c55abf5b83
DblK <admin@dblk.org>
2021-12-22 18:36:33 +0100

    Add some tests

--- gamescollection/collection_test.go
+++ gamescollection/collection_test.go
@@ -41,4 +41,41 @@ var _ = Describe("Collection", func() {
 			Expect(games.Files).To(HaveLen(1))
 		})
 	})
+	Describe("RemoveGame", func() {
+		BeforeEach(func() {
+			collection.ResetGamesCollection()
+		})
+		It("Removing existing ID", func() {
+			newGames := make([]repository.FileDesc, 0)
+			newFile := repository.FileDesc{
+				Size:     42,
+				Path:     "here",
+				GameID:   "0000000000000001",
+				GameInfo: "[0000000000000001][v0].nsp",
+				HostType: repository.LocalFile,
+			}
+			newGames = append(newGames, newFile)
+			collection.AddNewGames(newGames)
+
+			Expect(collection.Games().Files).To(HaveLen(1))
+			collection.RemoveGame("0000000000000001")
+			Expect(collection.Games().Files).To(HaveLen(0))
+		})
+		It("Removing not existing ID", func() {
+			newGames := make([]repository.FileDesc, 0)
+			newFile := repository.FileDesc{
+				Size:     42,
+				Path:     "here",
+				GameID:   "0000000000000001",
+				GameInfo: "[0000000000000001][v0].nsp",
+				HostType: repository.LocalFile,
+			}
+			newGames = append(newGames, newFile)
+			collection.AddNewGames(newGames)
+
+			Expect(collection.Games().Files).To(HaveLen(1))
+			collection.RemoveGame("0000000000000002")
+			Expect(collection.Games().Files).To(HaveLen(1))
+		})
+	})
 })
