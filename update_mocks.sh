c2cc87c608ca33e8c9532e54f14ffda3a9a4ec4f
DblK <admin@dblk.org>
2021-12-23 14:28:15 +0100

    Add new mock for Source

--- update_mocks.sh
+++ update_mocks.sh
@@ -1,2 +1,3 @@
 mkdir -p mock_repository
 mockgen github.com/DblK/tinshop/repository Config > mock_repository/mock_config.go 
+mockgen github.com/DblK/tinshop/repository Source > mock_repository/mock_source.go 
