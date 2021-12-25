d2ae1e3ed5e9d0a2e4135e54759961a28d05a0d8
DblK <admin@dblk.org>
2021-12-25 18:34:21 +0100

    Add reading file entry

--- sources/directory/tmp.go
+++ sources/directory/tmp.go
@@ -3,13 +3,19 @@ package directory
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/hex"
+	"errors"
 	"fmt"
 	"os"
+	"unsafe"
 
 	collection "github.com/DblK/tinshop/gamescollection"
 	"github.com/DblK/tinshop/repository"
 )
+
+type nsp_file struct {
+	Header    pfs0_header
+	FileEntry []pfs0_file_entry
+}
 
 type pfs0_header struct {
 	// magic uint32
@@ -19,38 +25,58 @@ type pfs0_header struct {
 	Reserved       uint32
 }
 
+type pfs0_file_entry struct {
+	File_offset     uint64
+	File_size       uint64
+	Filename_offset uint32
+	Reserved        uint32
+}
+
 func nspCheck(file repository.FileDesc) {
 	fmt.Println("GameID:", file.GameID)
 	key := collection.GetKey(file.GameID)
 	fmt.Println("Key:", key)
 	fmt.Println()
 
-	f, err := os.Open(file.Path)
+	err := openNSP(file.Path)
+	if err != nil {
+		fmt.Println("Error while opening NSP", err)
+	}
+}
+
+func openNSP(file string) error {
+	f, err := os.Open(file)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer f.Close()
 
-	// Working
-	// nspHeader := &pfs0_header{}
-	// binary.Read(f, binary.LittleEndian, &nspHeader.magic)
-	// binary.Read(f, binary.LittleEndian, &nspHeader.file_cnt)
-	// binary.Read(f, binary.LittleEndian, &nspHeader.str_table_size)
-	// binary.Read(f, binary.LittleEndian, &nspHeader.reserved)
+	newNSP := &nsp_file{}
 
-	// head := make([]byte, 4)
-	// f.Read(head)
-
+	// Read Header
 	nspHeader := pfs0_header{}
-	// data := make([]byte, 4)
-	data := make([]byte, 4*4)
+	data := make([]byte, unsafe.Sizeof(nspHeader))
 	_, _ = f.Read(data)
-	fmt.Println(len(data))
 	buffer := bytes.NewBuffer(data)
-	fmt.Println(buffer)
-	fmt.Printf("%s", hex.Dump(data))
-	err = binary.Read(buffer, binary.LittleEndian, &nspHeader)
+	_ = binary.Read(buffer, binary.LittleEndian, &nspHeader)
+	newNSP.Header = nspHeader
 
-	// fmt.Println(string(head))
-	fmt.Println(nspHeader)
+	if string(newNSP.Header.Magic[:]) != "PFS0" {
+		return errors.New("header Magic is not present")
+	}
+	// fmt.Println(nspHeader)
+
+	// Read file entry
+	for i := 0; i < int(nspHeader.File_cnt); i++ {
+		nspEntry := pfs0_file_entry{}
+		data := make([]byte, unsafe.Sizeof(nspEntry))
+		_, _ = f.Read(data)
+		buffer := bytes.NewBuffer(data)
+		_ = binary.Read(buffer, binary.LittleEndian, &nspEntry)
+		newNSP.FileEntry = append(newNSP.FileEntry, nspEntry)
+	}
+	fmt.Println(len(newNSP.FileEntry))
+	fmt.Println(newNSP.FileEntry)
+
+	return nil
 }
