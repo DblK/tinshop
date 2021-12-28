08aaad3f9b99883817d664e9ea4419c8b6f76e5c
DblK <admin@dblk.org>
2021-12-28 12:23:07 +0100

    Beginning to add check for nfs

--- sources/nfs/private.go
+++ sources/nfs/private.go
@@ -1,12 +1,15 @@
 package nfs
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"path/filepath"
 	"strings"
 
 	"github.com/DblK/tinshop/config"
 	collection "github.com/DblK/tinshop/gamescollection"
+	"github.com/DblK/tinshop/nsp"
 	"github.com/DblK/tinshop/repository"
 	"github.com/DblK/tinshop/utils"
 	"github.com/vmware/go-nfs-client/nfs"
@@ -14,19 +17,25 @@ import (
 	"github.com/vmware/go-nfs-client/nfs/util"
 )
 
+func getHostTarget(share string) (string, string, error) {
+	shareInfos := strings.Split(share, ":")
+
+	if len(shareInfos) != 2 {
+		return "", "", errors.New("Error parsing the nfs share configuration " + share)
+	}
+	return shareInfos[0], shareInfos[1], nil
+}
+
 func loadGamesNfs(share string) {
 	if config.GetConfig().DebugNfs() {
 		util.DefaultLogger.SetDebug(true)
 	}
 
-	shareInfos := strings.Split(share, ":")
-	if len(shareInfos) != 2 {
-		log.Printf("Error parsing the nfs share configuration (%s)\n", share)
+	host, target, err := getHostTarget(share)
+	if err != nil {
+		log.Println(err)
 		return
 	}
-
-	host := shareInfos[0]
-	target := shareInfos[1]
 
 	log.Printf("Loading games from nfs (host=%s target=%s)\n", host, target)
 
@@ -83,6 +92,10 @@ func lookIntoNfsDirectory(v *nfs.Target, share, path string) []repository.FileDe
 				if path != "." {
 					nfsRootPath += path
 				}
+				extension := filepath.Ext(dir.FileName)
+				if extension != ".nsp" && extension != ".nsz" {
+					continue
+				}
 
 				newFile := repository.FileDesc{Size: dir.Size(), Path: nfsRootPath + "/" + dir.FileName}
 				names := utils.ExtractGameID(dir.FileName)
@@ -91,10 +104,15 @@ func lookIntoNfsDirectory(v *nfs.Target, share, path string) []repository.FileDe
 					newFile.GameID = names.ShortID()
 					newFile.GameInfo = names.FullID()
 					newFile.HostType = repository.NFSShare
-					newGameFiles = append(newGameFiles, newFile)
 
 					if config.GetConfig().VerifyNSP() {
-						fmt.Println("VerifyNSP: NFS", newFile.Path)
+						valid, errTicket := nspCheck(newFile)
+						// fmt.Println(newFile.Path, valid, errTicket)
+						if valid || (errTicket != nil && errTicket.Error() == "TitleDBKey for game "+newFile.GameID+" is not found") {
+							newGameFiles = append(newGameFiles, newFile)
+						}
+					} else {
+						newGameFiles = append(newGameFiles, newFile)
 					}
 				} else {
 					log.Println("Ignoring file because parsing failed", dir.FileName)
@@ -114,4 +132,42 @@ func computePath(path string, dir *nfs.EntryPlus) string {
 		newPath = path + "/" + dir.FileName
 	}
 	return newPath
+}
+
+func nspCheck(file repository.FileDesc) (bool, error) {
+
+	key, err := collection.GetKey(file.GameID)
+	if err != nil {
+		return false, err
+	}
+
+	host, target, err := getHostTarget(file.Path)
+	if err != nil {
+		return false, err
+	}
+
+	fmt.Println(host, target)
+
+	return true, nil
+
+	mount, v := nfsConnect(host, target)
+	defer mount.Close()
+	defer v.Close()
+
+	f, err := v.Open(file.Path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	log.Println("Verifying Ticket:", file.Path)
+	valid, err := nsp.IsTicketValid(f, key)
+	if err != nil {
+		return false, err
+	}
+	if !valid {
+		return false, errors.New("Your file" + file.Path + "is not valid!")
+	}
+
+	return valid, err
 }
