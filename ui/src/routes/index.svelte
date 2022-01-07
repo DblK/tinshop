83ea0b7dd7035c4a5aeee42f61326559385dd2c3
DblK <admin@dblk.org>
2022-01-07 21:33:32 +0100

    Make some tests

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,6 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<script context="module">
+<script>
 	export const prerender = true;
+    import { onMount } from 'svelte';
+
+    let stats;
+    onMount(async () => {
+        const res = await fetch("http://localhost:3000/api/stats");
+        const JSONStats = await res.json();
+        stats = JSON.stringify(JSONStats)
+    })
 </script>
+
+<style>
+    .icon {
+        width: 64px;
+    }
+</style>
+
+<main>
+    <img class="icon" src="/favicon.png" alt="tinshop">
+    
+    <p>tinshop</p>
+    
+    <p>{stats}</p>
+</main>
